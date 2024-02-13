8128a5b7648bf51cd47aedacb11a1d3a75b92eb2
mobigaurav <meetgaurav1@gmail.com>
2024-02-12 21:34:55 -0500

    Animations project

---
+++ RiveAnimations/RiveAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveAnimationsApp.swift
+//  RiveAnimations
+//
+//  Created by Gaurav Kumar on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct RiveAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
